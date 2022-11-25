3ada25230b0e874a39e01b55e71a960e3108c908
mart1d4 <mart1danton@gmail.com>
2022-11-26 00:26:52 +0100

    Adds README along with stats

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
 import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 };
 
 export default App
