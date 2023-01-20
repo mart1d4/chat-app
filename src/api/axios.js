d9f89a95245b559965570e9e182056bfa1ec791c
mart1d4 <mart1danton@gmail.com>
2023-01-20 23:04:19 +0100

    Makes axios BASE_URL a env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3000/api';
 
 export default axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.BASE_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 });
