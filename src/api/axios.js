5b64d7366f27c74cecf6194f3ebf486869449700
mart1d4 <mart1danton@gmail.com>
2023-01-20 23:13:43 +0100

    Vercel test

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: "https://www.unthrust.com/api",
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: "https://www.unthrust.com/api",
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 });
