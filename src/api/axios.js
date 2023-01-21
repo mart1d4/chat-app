fc9be4b71e10defd7fb4879047a226a22d5e65ff
mart1d4 <mart1danton@gmail.com>
2023-01-21 04:06:04 +0100

    ""

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.unthrust.com/api';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '/api';
 
 export default axios.create({
     baseURL: BASE_URL,
