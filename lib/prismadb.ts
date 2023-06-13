13101a197ce5f0d577a1b112fbb7713e33230de3
mart1d4 <mart1danton@gmail.com>
2023-06-13 08:10:00 +0200

    .

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        log: ['query'],
+        log: ['query', 'info', 'warn'],
     });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
